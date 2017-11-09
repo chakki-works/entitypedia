1c5f44582f95b79a6bbb35cd51e4c78c2c82cde7
Hironsan <light.tree.1.13@gmail.com>
2017-11-09 15:21:41 +0900

    Add political_organization

--- tests/test_coordinator.py
+++ tests/test_coordinator.py
@@ -34,11 +34,52 @@ class TestCoordinator(unittest.TestCase):
 
 class Name(TestCoordinator):
 
+    def test_name_other(self):
+        pass
+
+    def test_person(self):
+        pass
+
     def test_god(self):
         self.execute('name/god')
 
 
 class Organization(TestCoordinator):
+
+    """
+    def test_organizaton_other(self):
+        self.execute('organization/organizaton_other')
+
+    def test_international_organization(self):
+        self.execute('organization/international_organization')
+
+    def test_show_organization(self):
+        self.execute('organization/show_organization')
+
+    def test_family(self):
+        self.execute('organization/family')
+
+    def test_ethnic_group_other(self):
+        self.execute('organization/ethnic_group/ethnic_group_other')
+
+    def test_nationality(self):
+        self.execute('organization/ethnic_group/nationality')
+
+    def test_sports_organization_other(self):
+        self.execute('organization/sports_organization/sports_organization_other')
+
+    def test_pro_sports_organization(self):
+        self.execute('organization/sports_organization/pro_sports_organization')
+
+    def test_sports_league(self):
+        self.execute('organization/sports_organization/sports_league')
+
+    def test_corporation_other(self):
+        self.execute('organization/corporation/corporation_other')
+
+    def test_compnay_group(self):
+        self.execute('organization/corporation/compnay_group')
+    """
 
     def test_company(self):
         self.execute('organization/corporation/company')
@@ -52,17 +93,14 @@ class Organization(TestCoordinator):
     def test_military(self):
         self.execute('organization/political_organization/military')
 
+    def test_government(self):
+        self.execute('organization/political_organization/government')
+
+    def test_political_organization_other(self):
+        self.execute('organization/political_organization/political_organization_other')
+
 
 class Location(TestCoordinator):
-
-    def test_country(self):
-        self.execute('location/gpe/country')
-
-    def test_county(self):
-        self.execute('location/gpe/county')
-
-    def test_province(self):
-        self.execute('location/gpe/province')
 
     def test_spa(self):
         self.execute('location/spa')
@@ -70,23 +108,14 @@ class Location(TestCoordinator):
     def test_city(self):
         self.execute('location/gpe/city')
 
-    def test_mountain(self):
-        self.execute('location/geological_region/mountain')
+    def test_county(self):
+        self.execute('location/gpe/county')
 
-    def test_lake(self):
-        self.execute('location/geological_region/lake')
+    def test_province(self):
+        self.execute('location/gpe/province')
 
-    def test_bay(self):
-        self.execute('location/geological_region/bay')
-
-    def test_sea(self):
-        self.execute('location/geological_region/sea')
-
-    def test_island(self):
-        self.execute('location/geological_region/island')
-
-    def test_river(self):
-        self.execute('location/geological_region/river')
+    def test_country(self):
+        self.execute('location/gpe/country')
 
     def test_continental_region(self):
         self.execute('location/region/continental_region')
@@ -94,17 +123,35 @@ class Location(TestCoordinator):
     def test_domestic_region(self):
         self.execute('location/region/domestic_region')
 
+    def test_mountain(self):
+        self.execute('location/geological_region/mountain')
+
+    def test_island(self):
+        self.execute('location/geological_region/island')
+
+    def test_river(self):
+        self.execute('location/geological_region/river')
+
+    def test_lake(self):
+        self.execute('location/geological_region/lake')
+
+    def test_sea(self):
+        self.execute('location/geological_region/sea')
+
+    def test_bay(self):
+        self.execute('location/geological_region/bay')
+
     def test_astral_body_other(self):
         self.execute('location/astral_body/astral_body_other')
-
-    def test_constellation(self):
-        self.execute('location/astral_body/constellation')
 
     def test_planet(self):
         self.execute('location/astral_body/planet')
 
     def test_star(self):
         self.execute('location/astral_body/star')
+
+    def test_constellation(self):
+        self.execute('location/astral_body/constellation')
 
 
 class Facility(TestCoordinator):
@@ -121,29 +168,38 @@ class Facility(TestCoordinator):
     def test_school(self):
         self.execute('facility/goe/school')
 
-    def test_market(self):
-        self.execute('facility/goe/market')
-
     def test_research_institute(self):
         self.execute('facility/goe/research_institute')
 
-    def test_car_stop(self):
-        self.execute('facility/goe/car_stop')
+    def test_market(self):
+        self.execute('facility/goe/market')
 
-    def test_amusement_park(self):
-        self.execute('facility/goe/amusement_park')
+    def test_park(self):
+        self.execute('facility/goe/park')
 
-    def test_zoo(self):
-        self.execute('facility/goe/zoo')
-
-    def test_theater(self):
-        self.execute('facility/goe/theater')
+    def test_sports_facility(self):
+        self.execute('facility/goe/sports_facility')
 
     def test_museum(self):
         self.execute('facility/goe/museum')
 
+    def test_zoo(self):
+        self.execute('facility/goe/zoo')
+
+    def test_amusement_park(self):
+        self.execute('facility/goe/amusement_park')
+
+    def test_theater(self):
+        self.execute('facility/goe/theater')
+
     def test_worship_place(self):
         self.execute('facility/goe/worship_place')
+
+    def test_car_stop(self):
+        self.execute('facility/goe/car_stop')
+
+    def test_station(self):
+        self.execute('facility/goe/station')
 
     def test_airport(self):
         self.execute('facility/goe/airport')
@@ -151,23 +207,14 @@ class Facility(TestCoordinator):
     def test_port(self):
         self.execute('facility/goe/port')
 
-    def test_park(self):
-        self.execute('facility/goe/park')
-
-    def test_station(self):
-        self.execute('facility/goe/station')
-
-    def test_sports_facility(self):
-        self.execute('facility/goe/sports_facility')
-
-    def test_canal(self):
-        self.execute('facility/line/canal')
+    def test_railroad(self):
+        self.execute('facility/line/railroad')
 
     def test_road(self):
         self.execute('facility/line/road')
 
-    def test_railroad(self):
-        self.execute('facility/line/railroad')
+    def test_canal(self):
+        self.execute('facility/line/canal')
 
     def test_tunnel(self):
         self.execute('facility/line/tunnel')
@@ -196,11 +243,11 @@ class Product(TestCoordinator):
 
 class Event(TestCoordinator):
 
-    def test_earthquake(self):
-        self.execute('event/natural_phenomenon/earthquake')
-
     def test_war(self):
         self.execute('event/incident/war')
+
+    def test_earthquake(self):
+        self.execute('event/natural_phenomenon/earthquake')
 
 
 class NaturalObject(TestCoordinator):
