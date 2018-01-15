fd681fde7ab0a96388c5344bc02192e84b2e323b
Hironsan <hiroki.nakayama.py@gmail.com>
2018-01-15 10:08:15 +0900

    Update test_extractor

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from entitypedia.corpora.wikipedia.extractor import extract_abstracts, extract_paragraphs
@@ -9,13 +10,14 @@ class TestExtractor(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.files = get_file_list('../data/extracted')
+        cls.save_dir = os.path.join(os.path.dirname(__file__), '../data/interim')
 
     def test_extract_abstracts(self):
-        abstract_file = 'abstracts.jsonl'
+        abstract_file = os.path.join(self.save_dir, 'abstracts.jsonl')
         abstracts = extract_abstracts(self.files)
         save_jsonl(abstracts, abstract_file)
 
     def test_extract_paragraphs(self):
-        paragraph_file = 'paragraphs.jsonl'
+        paragraph_file = os.path.join(self.save_dir, 'paragraphs.jsonl')
         paragraphs = extract_paragraphs(self.files)
         save_jsonl(paragraphs, paragraph_file)
